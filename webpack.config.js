4d9a9fb3d055d20d2799cac88b52ab47c224ef44
Daniela Michelle <danielamij251@gmail.com>
2020-09-10 22:38:12 -0500

    EsLint

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+// SASS => npm install --save-dev mini-css-extract-plugin css-loader node-sass sass-loader
+// WEBPACK =>  npm install webpack webpack-cli html-webpack-plugin html-loader  --save-dev
+// DEV-SERVER =>  npm install --save-dev webpack-dev-server
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
+};
